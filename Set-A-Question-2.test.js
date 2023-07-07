a63ed1f5ca2451d896c458e6ba74161064265efa
Michael Aurilia <michaelaurilia919@gmail.com>
2023-07-07 15:52:30 -0400

    still fixing duplicates

--- Set-A-Question-2.test.js
+++ Set-A-Question-2.test.js
@@ -1,8 +1,8 @@
 const findAllDuplicates = require("./Set-A-Question-2");
 
 describe("Test cases", () => {
-    test("[4,3,2,7,8,2,3,1] => [2,3]", () => {
-      expect(findAllDuplicates([4,3,2,7,8,2,3,1])).toEqual([2,3]);
+    test("[4,3,2,7,7,2,3,1] => [2,3]", () => {
+      expect(findAllDuplicates([4,3,2,7,8,2,3,1])).toEqual([2,3,7]);
     })
     test("[1,1,2] => [1]", () => {
       expect(findAllDuplicates([1,1,2])).toEqual([1]);
